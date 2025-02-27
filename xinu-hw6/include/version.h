fff5f3108a508402357584a5346736973f6ba594
Justin Hoffman <jhoffman89@androzani.mscsnet.mu.edu>
2025-02-27 16:00:01 -0600

    Added Project 6

---
+++ xinu-hw6/include/version.h
@@ -0,0 +1,1 @@
+#define VERSION "(Embedded Xinu) (nezhaobj) #86 (lbondi1@kastria.mscsnet.mu.edu) Mon Feb 24 09:00:10 PM CST 2025"
---
+++ xinu-hw6/system/dispatch.c
@@ -0,0 +1,55 @@
+/**
+ * @file dispatch.c
+ * @provides dispatch
+ *
+ * COSC 3250 - Project 5
+ * Handles traps
+ * @author Justin Hoffman & Luke Bondi
+ * Instructor Dr.Brylow
+ * TA-BOT:MAILTO justin.hoffman@marquette.edu
+ *
+ */
+/* Embedded XINU, Copyright (C) 2008, 2023.  All rights reserved. */
+
+
+#include <xinu.h>
+
+/**
+ * @ingroup process
+ * Dispatch the trap or exception handler, called via interrupt.S
+ * @param cause  The value of the scause register 
+ * @param stval  The value of the stval register  
+ * @param frame  The stack pointer of the process that caused the interupt 
+ * @param program_counter  The value of the sepc register 
+ */
+
+void dispatch(ulong cause, ulong val, ulong *frame, ulong *program_counter) {
+    ulong swi_opcode;
+    swi_opcode = frame[CTX_A7];
+    if((long)cause > 0) {
+        cause = cause << 1;
+        cause = cause >> 1;
+	
+	if ((long)cause == E_ENVCALL_FROM_UMODE)
+       	{
+		frame[CTX_A0] = syscall_dispatch(swi_opcode, (ulong*)&frame[CTX_A0]);
+		set_sepc((ulong)program_counter + (ulong)4);
+	}
+       	else
+       	{	
+		xtrap(frame, cause, val, program_counter);
+	}
+
+       /**
+	* TODO:
+	* Check to ensure the trap is an environment call from U-Mode
+	* Find the system call number that's triggered
+	* Pass the system call number and any arguments into syscall_dispatch.
+	* Make sure to set the return value in the appropriate spot.
+	* Update the program counter appropriately with set_sepc
+	*
+	* If the trap is not an environment call from U-Mode call xtrap
+	*/
+    }
+}
+
