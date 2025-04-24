bff19f6797d2c6a49b8ea60573316a656be06a26
Luke Bondi <lbondi1@telos.mscsnet.mu.edu>
2025-04-24 18:27:14 -0500

    more sb_freelist

--- xinu-hw9/file/sbFreeBlock.c
+++ xinu-hw9/file/sbFreeBlock.c
@@ -41,5 +41,44 @@ devcall sbFreeBlock(struct superblock *psuper, int block)
 	wait(psuper->sb_freelock);
 	struct fbcnode *fbc = psuper->sb_freelist;
 
+	if(fbc == NULL) //case 1
+	{
+
+		struct fbcnode *newfbc = (fbcnode *)malloc(sizeof(fbcnode));
+
+		newfbc->fbc_blocknum = block;
+		newfbc->count = 0;
+		newfbc->fbc_next = NULL;
+		psuper->sb_freelist = newfbc;
+
+		//call swizzle somewhere
+		//signal the lock and then return 
+		//for all cases do steps 1 and 2 
+		//need to set up the super block 
+		//other than that set up very similar to case 2 
+
+
+	}
+
+
+	while(fbc->next != NULL)
+	{
+		fbc = fbc->next;
+	}
+
+	if(fbc->fbc_count == FREEBLOCKMAX || fbc->fbc_count == 0) //case 2
+	{
+		//malloc to grab another block of space freeblock struct
+		struct fbcnode *newfbc = (fbcnode *)malloc(sizeof(fbcnode));
+		newfbc->fbc_blocknum = block;
+		newfbc->fbc_count = 0;
+		newfbc->fbc_next = NULL;
+		fbc->next = newfbc;
+	}
+
+
+
+
+
     return SYSERR;
 }
