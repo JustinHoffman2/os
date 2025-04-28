a8d0005b11541601c9e51f1b7938b97a18783979
Justin Hoffman <justin.hoffman@marquette.edu>
2025-04-28 14:26:34 -0500

    Added Testing

--- xinu-hw9/shell/xsh_test.c
+++ xinu-hw9/shell/xsh_test.c
@@ -7,6 +7,7 @@
 
 #include <xinu.h>
 
+void printFreeList(void);
 /**
  * Shell command (test) is testing hook.
  * @param args array of arguments
@@ -14,7 +15,44 @@
  */
 command xsh_test(int nargs, char *args[])
 {
-    //TODO: Test your O/S.
-    printf("This is where you should put some testing code.\n");
+    int block = 0, i = 0;
+
+    printFreeList();
+    while (i < DISKBLOCKTOTAL)
+    {
+        block = sbGetBlock(supertab);
+        printf("Block %d = %d\n", i, block);
+        sbFreeBlock(supertab, block);
+        printFreeList();
+        if ((i < 5) || ((i > 54) && (i < 63)))
+        {
+            int c = getc(CONSOLE);
+        }
+    }
+    
     return OK;
+}
+
+void printFreeList(void);
+{
+    int i;
+    struct fbcnode *fbc = supertab->sb_freelst;
+    while (fbc != NULL)
+    {
+        printf("Freelist Collector Nodes:\n");
+        printf("Blk\t%d,\tcnt\t%d = ", fbc->fbc_blocknum, fbc->fbc_count);
+        for (i = 0; i < 10 || i < fbc->fbc_count; i++)
+        {
+            printf("[%03d]", fbc->fbc_free[i]);
+        }
+        if (i == 10)
+        {
+            printf("...[%03d]\n", fbc->fbc_free[fbc->fbc_count - 1]);
+        } 
+        else
+        {
+            printf("\n");
+        }
+        fbc = fbc->fbc_next;
+    }
 }
