471678722785c7014971aa694cc7829aadc5ad36
Justin Hoffman <jhoffman89@telos.mscsnet.mu.edu>
2025-04-28 21:20:04 -0500

    Updated sbFreeblock.c

--- xinu-hw9/file/sbFreeBlock.c
+++ xinu-hw9/file/sbFreeBlock.c
@@ -22,79 +22,91 @@ devcall sbFreeBlock(struct superblock *psuper, int block)
     //  mutually exclusive access to the free list, and write
     //  the changed free list segment(s) back to disk.
     //
-
 	int diskfd;
 	struct dentry *phw;
-	if(psuper == NULL)
+	
+	if (psuper == NULL)
 	{
 		return SYSERR;
 	}
 	phw = psuper->sb_disk;
-	if(phw != NULL)
+	if (phw == NULL)
 	{
 		return SYSERR;
 	}
 
-	if(block <= 0 && block > DISKBLOCKTOTAL)
+	if (block <= 0 && block > DISKBLOCKTOTAL)
 	{
 	       return SYSERR;
-	}	       
+	}
+
 	diskfd = phw - devtab;
 	wait(psuper->sb_freelock);
 	struct fbcnode *fbc = psuper->sb_freelst;
 
-	if(fbc == NULL) //case 1
+	if (fbc == NULL) // Case 1
 	{
+		struct dirblock *swizzleSB;
 		struct fbcnode *newfbc = (struct fbcnode *)malloc(sizeof(struct fbcnode));
 		newfbc->fbc_blocknum = block;
 		newfbc->fbc_count = 0;
 		newfbc->fbc_next = NULL;
 		psuper->sb_freelst = newfbc;
 
-		if (swizzle(fbc, diskfd) == NULL)
+		if (swizzle(newfbc, diskfd) == NULL)
 		{
 			return SYSERR;
 		}
+		// Write superblock to disk
+		swizzleSB = psuper->sb_dirlst;
+		psuper->sb_dirlst = (struct dirblock *)swizzleSB->db_blocknum;
+		seek(diskfd, psuper->sb_blocknum);
+		if (write(diskfd, psuper, sizeof(struct superblock)) == SYSERR)
+		{
+			return SYSERR;
+		}
+		psuper->sb_dirlst = swizzleSB;
+
 		signal(psuper->sb_freelock);
-		return OK;
-		//signal the lock and then return 
-		//for all cases do steps 1 and 2 
-		//need to set up the super block 
-		//other than that set up very similar to case 2 
+		return OK; 
 	}
 
-	while(fbc->fbc_next != NULL)
+	while (fbc->fbc_next != NULL)
 	{
 		fbc = fbc->fbc_next;
 	}
 
-	if(fbc->fbc_count == FREEBLOCKMAX || fbc->fbc_count == 0) //case 2
+	if (fbc->fbc_count == FREEBLOCKMAX || fbc->fbc_count == 0) // Case 2
 	{
-		//malloc to grab another block of space freeblock struct
+		// Malloc to get enough space to make a new collector node
 		struct fbcnode *newfbc = (struct fbcnode *)malloc(sizeof(struct fbcnode));
 		newfbc->fbc_blocknum = block;
 		newfbc->fbc_count = 0;
 		newfbc->fbc_next = NULL;
 		fbc->fbc_next = newfbc;
 
-		if (swizzle(fbc, diskfd) == NULL)
+		if (swizzle(fbc, diskfd) == NULL || swizzle(newfbc, diskfd) == NULL)
 		{
 			return SYSERR;
 		}
+
 		signal(psuper->sb_freelock);
 		return OK;
 	}
 	
-	//case 3
+	// Case 3
 	fbc->fbc_free[fbc->fbc_count] = block;
 	fbc->fbc_count++;
 	if (swizzle(fbc, diskfd) == NULL)
 	{
 		return SYSERR;
 	}
+
+	signal(psuper->sb_freelock);
 	return OK;
 }
 
+// Writes fbc to disk
 int swizzle(struct fbcnode *fbc, int diskfd) {
 	struct fbcnode *fbc2 = fbc->fbc_next;
 	if (fbc2 == NULL)
@@ -103,13 +115,14 @@ int swizzle(struct fbcnode *fbc, int diskfd) {
 	}
 	else
 	{
-		fbc->fbc_next = (struct fbcnode *)fbc->fbc_next->fbc_blocknum;
+		fbc->fbc_next = (struct fbcnode *)fbc2->fbc_blocknum;
 	}
 	seek(diskfd, fbc->fbc_blocknum);
 	if (write(diskfd, fbc, sizeof(struct fbcnode)) == SYSERR)
 	{
 		return SYSERR;
 	}
+	fbc->fbc_next = fbc2;
 	return OK;
 	
 }
